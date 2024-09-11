1d6200a9d0a2a65271d0acbcf5bd14cb7a04ef44
Happypikachoo <106103630+happypikchoo@users.noreply.github.com>
2024-09-11 12:35:02 -0700

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-   public static void main (String[] args) {
-       //output some text to terminal:
-    System.out.println("Hello World!");
-       System.out.print("print is on the same line:0");
-       System.out.println("println is not on the same line>:(");
-   }
-}
