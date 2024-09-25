bb67eb5703800ff857aaafd8a424b8b79c8b6082
Happypikachoo <106103630+happypikchoo@users.noreply.github.com>
2024-09-25 12:21:40 -0700

    Add files via upload

---
+++ Haiku.java
@@ -0,0 +1,9 @@
+public class Haiku {
+    public static void main (String[] args) {
+        //output some text to terminal:
+        System.out.println("I like computers");
+        System.out.println("AP Comp Sci is awesome");
+        System.out.println("I also like math");
+
+    }
+}
