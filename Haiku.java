b1360ce251c340dcde0b697f0ee6470ebb94bfe0
Happypikachoo <106103630+happypikchoo@users.noreply.github.com>
2024-09-26 13:22:52 -0700

    Delete Haiku.java

--- Haiku.java
+++
@@ -1,9 +0,0 @@
-public class Haiku {
-    public static void main (String[] args) {
-        //output some text to terminal:
-        System.out.println("I like computers");
-        System.out.println("AP Comp Sci is awesome");
-        System.out.println("I also like math");
-
-    }
-}
