b967ec8aecb566bbd201745dc1ab4bb3d724b6c4
Happypikachoo <106103630+happypikchoo@users.noreply.github.com>
2024-10-16 12:46:48 -0700

    Add files via upload

--- Elby.java
+++ Elby.java
@@ -8,6 +8,88 @@ public class Elby {
         return "Hello, let's talk.";
     }
 
+    /**
+     * Take a statement with "you <something> me" and transform it into "What makes
+     * you think that I <something> you?"
+     *
+     * @param statement the user statement, assumed to contain "you" followed by "me"
+     * @return the transformed statement
+     */
+
+    public String transformYouMeStatement(String statement) {
+        //Remove the final period, if there is one
+        statement = statement.trim();
+        String lastChar = statement.substring(statement.length() - 1);
+        if (lastChar.equals(".")) {
+            statement = statement.substring(0, statement.length() - 1);
+        }
+
+        int posOfYou = findKeyword(statement, "you", 0);
+        int posOfMe  = findKeyword(statement, "me",  posOfYou + 3);
+
+        String restOfStatement = statement.substring(posOfYou + 3, posOfMe).trim();
+        return "What makes you think that I " + restOfStatement + " you?";
+    }
+
+    /**
+     * Take a statement with "I want to <something>." and transform it into "What
+     * would it mean to <something>?"
+     *
+     * @param statement the user statement, assumed to contain "I want to"
+     * @return the transformed statement
+     */
+
+    public String transformIWantToStatement(String statement) {
+        //Remove the final period, if there is one
+        statement = statement.trim();
+        String lastChar = statement.substring(statement.length() - 1);
+        if (lastChar.equals(".")) {
+            statement = statement.substring(0, statement.length() - 1);
+        }
+        int pos = findKeyword(statement, "I want to", 0);
+        String restOfStatement = statement.substring(pos + 9).trim();
+        return "What would it mean to " + restOfStatement + "?";
+    }
+
+    /**
+     * Take a statement with "I want <something>." and transform it
+     * into Would you really be happy if you had <something>?
+     *
+     * @param statement the user statement, assumed to contain "I want"
+     * @return the transformed statement
+     */
+    public String transformIWantStatement(String statement){
+        statement = statement.trim();
+        String lastChar = statement.substring(statement.length() - 1);
+        if (lastChar.equals(".")) {
+            statement = statement.substring(0, statement.length() - 1);
+        }
+        int pos = findKeyword(statement, "I want", 0);
+        String restOfStatement = statement.substring(pos+6).trim();
+        return "Would you really be happy if you had " + restOfStatement;
+    }
+
+    /**
+     * Take a statement with "I <something> you" and transform it into
+     * "Why do you <something> me?"
+     *
+     * @param statement the user statement, assumed to contain "I" followed by
+     * 	something "you"
+     * @return the transformed statement
+     */
+    public String transformIMeStatement(String statement) {
+        statement = statement.trim();
+        String lastchar = statement.substring(statement.length()-1);
+        if (lastchar.equals(".")) {
+            statement = statement.substring(0, statement.length() - 1);
+        }
+        int startpos = findKeyword(statement, "I",  0);
+        int endpos = findKeyword(statement, "you",  startpos+1);
+        String restofStatement = statement.substring(startpos+1, endpos).trim();
+        return "Why do you " + restofStatement + " me";
+    }
+
+
 
     /**
      * Search for one word in phrase. The search is not case sensitive. This method
@@ -59,28 +141,38 @@ public class Elby {
     public String getResponse(String statement) {
         String response = "";
 
-        if (findKeyword(statement, "mother", 0) !=-1
-                || statement.indexOf("father") >= 0
-                || statement.indexOf("sister") >= 0
-                || statement.indexOf("brother") >= 0) {
+        if (findKeyword(statement, "you", 0) < findKeyword(statement, "me",  0) ) {
+            response = transformYouMeStatement(statement);
+        } else if (findKeyword(statement, "I want to", 0) !=-1)  {
+            response = transformIWantToStatement(statement);
+        } else if (findKeyword(statement, "I", 0) < findKeyword(statement, "you", 0)) {
+            response = transformIMeStatement(statement);
+        } else if (findKeyword(statement, "I want", 0) !=-1) {
+            response = transformIWantStatement(statement);
+        }
+       else if (findKeyword(statement, "mother", 0) !=-1
+                || findKeyword(statement, "father", 0) !=-1
+                || findKeyword(statement, "sister", 0) !=-1
+                || findKeyword(statement, "brother", 0) !=-1) {
             response = "Tell me more about your family.";
             //pets====================================
-        } else if (statement.indexOf("dog") >=0 || statement.indexOf("cat") >=0) {
+        } else if (findKeyword(statement, "dog", 0) !=-1
+        || findKeyword(statement, "cat", 0) !=-1) {
             response = "Tell me more about your pets";
             //my name====================================
-        } else if (statement.indexOf("Jonathan") >= 0) {
+        } else if (findKeyword(statement, "Jonathan", 0) !=-1) {
             response = "Jonathan is pretty good at coding";
             //no response ====================================
         } else if (statement.indexOf("") >= 0) {
             response = "Say something please";
             //extra 3 keywords====================================
-        } else if (statement.indexOf("care") >= 0) {
+        } else if (findKeyword(statement, "care", 0) !=-1) {
             response = "Yes!:D";
-        } else if (statement.indexOf("boom") >= 0) {
+        } else if (findKeyword(statement, "boom", 0) !=-1) {
             response = "That sounds dangerous>:(";
-        } else if (statement.indexOf("secret") >= 0) {
+        } else if (findKeyword(statement, "secret", 0) !=-1) {
             response = "You can count on me! I won't tell a single soul ;)";
-        }  if (statement.indexOf("no") >= 0) {
+        }  else if (findKeyword(statement, "no", 0) !=-1) {
             response = "Why so negative?";
         }
         else {
